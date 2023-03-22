5dd6fba616cd670288c0ddb5e3add35ea2d82fc5
Chikaodili Deng <mchikky@yahoo.com>
2023-03-22 11:09:53 +0100

    Initial Commit

---
+++ Xplore_Interface/Xplore_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xplore_InterfaceApp.swift
+//  Xplore_Interface
+//
+//  Created by Chikaodili Deng on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Xplore_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
